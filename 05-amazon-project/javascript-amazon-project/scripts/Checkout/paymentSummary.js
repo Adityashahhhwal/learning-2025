ee952ca72c1c767d5ff476192240b3d291e20d81
Aditya Kumar Shahwal <adityashahwal2004@gmail.com>
2026-02-10 22:31:38 +0530

    feat: enhance renderPaymentSummary function with price and delivery details

--- 05-amazon-project/javascript-amazon-project/scripts/Checkout/paymentSummary.js
+++ 05-amazon-project/javascript-amazon-project/scripts/Checkout/paymentSummary.js
@@ -1,10 +1,17 @@
 import {cart} from "../../data/cart.js";
-import {getMatchingProduct} from "../../data/products.js";
+import {getMatchingProduct,} from "../../data/products.js";
+
 export function renderPaymentSummary() {
     let html = "";
 
     cart.forEach(cartItem => {
         const matchingProduct = getMatchingProduct(cartItem.productId);
+        const priceText = formatPrice(matchingProduct.priceCents);
+        const deliveryOptionId = cartItem.deliveryOptionId || defaultDeliveryOptionId;
+        const matchingDeliveryOption = getMatchingDeliveryOption(deliveryOptionId);
+        const deliveryDate = getDeliveryDate(matchingDeliveryOption.deliveryDays);
         
+        html += `
+        `
     });
 }
