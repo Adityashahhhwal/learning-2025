909a4ff145205a51b837646f8a0213db06a564d0
Aditya Kumar Shahwal <adityashahwal2004@gmail.com>
2026-02-10 22:36:40 +0530

    Clean checkout.js comment

--- 05-amazon-project/javascript-amazon-project/scripts/checkout.js
+++ 05-amazon-project/javascript-amazon-project/scripts/checkout.js
@@ -2,4 +2,4 @@ import { renderCheckoutPage } from "./Checkout/orderSummary.js";
 
 renderCheckoutPage();
 
-// This file serves as the entry point for the checkout pagecd "c:\Users\user\Documents\GitHub\learning-2025\05-amazon-project\javascript-amazon-project"
+// This file serves as the entry point for the checkout page
