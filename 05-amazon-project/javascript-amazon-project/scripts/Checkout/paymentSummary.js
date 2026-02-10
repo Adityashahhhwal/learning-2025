36783f9420816e3714eb39e36c95ebed04720549
Aditya Kumar Shahwal <adityashahwal2004@gmail.com>
2026-02-10 05:30:47 +0530

    feat: implement renderPaymentSummary function and integrate it into checkout process

---
+++ 05-amazon-project/javascript-amazon-project/scripts/Checkout/paymentSummary.js
@@ -0,0 +1,4 @@
+export function renderPaymentSummary() {
+    let html = "";
+    console.log('hello');
+}
--- 05-amazon-project/javascript-amazon-project/scripts/checkout.js
+++ 05-amazon-project/javascript-amazon-project/scripts/checkout.js
@@ -1,2 +1,6 @@
 import { renderCheckoutPage } from "./Checkout/orderSummary.js";
+import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 renderCheckoutPage();
+renderPaymentSummary();
+
+// This file serves as the entry point for the checkout page, importing and rendering both the order summary and payment summary components.
