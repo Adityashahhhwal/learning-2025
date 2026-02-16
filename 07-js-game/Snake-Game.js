e7276cee3b5660c65117eef52dbcbbbab256259d
Aditya Kumar Shahwal <adityashahwal2004@gmail.com>
2026-02-16 16:56:38 +0530

    Refactor code structure for improved readability and maintainability

--- 07-js-game/Snake-Game.js
+++ 07-js-game/Snake-Game.js
@@ -1,19 +1,95 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext('2d'); 
-const gridSize = 20;
-const cellSize = canvas.width/gridSize;
-
-for (let i = 0; i <= gridSize; i++) {
-    ctx.beginPath();
-    ctx.moveTo(i*cellSize,0);
-    ctx.lineTo(i*cellSize,canvas.height);
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(0,i*cellSize);
-    ctx.lineTo(canvas.width,i*cellSize);
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
+const board = document.getElementById('game-board');
+const gridSize = 20; 
+const logo = document.getElementById('logo');
+const instructionText = document.getElementById('instruction-text');
+//define game variables
+let snake = [{x:2, y:10}];
+let food = getPosition();
+let direction = 'up';
+let gameSpeed = 200;
+let gameStarted = false;
+//draw snake
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+    drawFood();
 }
 
+function drawSnake() {
+    snake.forEach(position => {
+        const snakeElement = createGameElement('div', 'snake');
+        board.appendChild(snakeElement);
+        setPosition(snakeElement,position);
+    });
+}
+function drawFood() {
+    const foodElement = createGameElement('div','food');
+    board.appendChild(foodElement);
+    setPosition(foodElement,food);
+}
+
+function createGameElement(tag, className ){
+    const element = document.createElement(tag);
+    element.className = className;
+
+    return element;
+}
+
+// set the position of snake or food 
+
+function setPosition(snakeElement,position){
+    snakeElement.style.gridColumn = position.x; // 
+    snakeElement.style.gridRow = position.y; //
+}
+
+// draw();
+
+
+function getPosition() {
+    const x = Math.floor(Math.random()*gridSize)+1;
+    const y = Math.floor(Math.random()*gridSize)+1;
+
+    return {x,y};
+}
+
+// moving the snake
+
+function move(){
+    const head = {...snake[0]};
+
+    switch (direction) {
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+        case 'right':
+            head.x++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        default:
+            break;
+    }
+    snake.unshift(head);
+    
+    if (head.x == food.x && head.y == food.y){
+        food = getPosition();
+        clearInterval(loopID);
+    }else{
+        snake.pop();
+    }
+}
+
+let loopID = setInterval(() =>{
+    draw();
+    move();
+},gameSpeed);
+
+function startGame() {
+    instructionText.remove();
+    logo.remove();
+    gameStarted = true;
+}
